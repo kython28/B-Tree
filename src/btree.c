358b47b4d1419cdeb63595769bb64aea1f92e570
Enrique Mora <kike28.py@pm.me>
2022-04-14 17:40:43 +0400

    Hotfix - Fixing up some bugs and adding some new features

--- src/btree.c
+++ src/btree.c
@@ -1,14 +1,17 @@
 #include "btree.h"
 
-#if BTREE_DTYPE >= 8
+#if BTREE_DTYPE == 8 ||  BTREE_DTYPE == 9
 #include <math.h>
 #include <float.h>
 
 #if BTREE_DTYPE == 8
 #define BTREE_EPSILON FLT_EPSILON
-#else
+#elif BTREE_DTYPE == 9
 #define BTREE_EPSILON DBL_EPSILON
 #endif
+
+#elif BTREE_DTYPE == 10
+#include <sys/param.h>
 
 #endif
 
@@ -20,12 +23,24 @@ btree allocBtree(){
 void split_node(btree tree){
 	btree child1, child2, parent;
 	uint8_t run = 1, x;
+
+	#if BTREE_DTYPE == 10
+	void *key, **node_keys, **parent_keys, *k_tmp[2];
+	uint64_t *node_keys_len, *parent_keys_len, kl_tmp[2];
+	#else
 	btree_k_t key, *node_keys, *parent_keys, k_tmp[2];
+	#endif
+
 	void **node_childs, **node_values, *parent_childs[2], **parent_values;
 	while (1){
 		node_childs = tree->childs;
 		node_values = tree->values;
 		node_keys = tree->keys;
+
+		#if BTREE_DTYPE == 10
+		node_keys_len = tree->keys_len;
+		#endif
+
 		parent = tree->parent;
 
 		child1 = allocBtree();
@@ -54,6 +69,11 @@ void split_node(btree tree){
 			child1->keys[0] = node_keys[0];
 			child2->keys[0] = node_keys[2];
 
+			#if BTREE_DTYPE == 10
+			child1->keys_len[0] = node_keys_len[0];
+			child2->keys_len[0] = node_keys_len[2];
+			#endif
+
 			child1->values[0] = node_values[0];
 			child2->values[0] = node_values[2];
 
@@ -61,6 +81,11 @@ void split_node(btree tree){
 			node_values[0] = node_values[1];
 
 			memset(&node_keys[1], 0, sizeof(btree_k_t)*2);
+
+			#if BTREE_DTYPE == 10
+			memset(&node_keys_len[1], 0, 16);
+			#endif
+
 			memset(&node_values[1], 0, sizeof(void*)*2);
 			memset(node_childs, 0, sizeof(void*)*4);
 
@@ -69,6 +94,11 @@ void split_node(btree tree){
 			break;
 		}else{
 			child1->keys[0] = node_keys[2];
+			
+			#if BTREE_DTYPE == 10
+			child1->keys_len[0] = node_keys_len[2];
+			#endif
+
 			child1->values[0] = node_values[2];
 			child1->parent = parent;
 
@@ -85,6 +115,10 @@ void split_node(btree tree){
 			parent_keys = parent->keys;
 			parent_values = parent->values;
 
+			#if BTREE_DTYPE == 10
+			parent_keys_len = parent->keys_len;
+			#endif
+
 			if (node_childs[0] == tree){
 				
 				memcpy(parent_childs, &node_childs[1], 2*sizeof(void*));
@@ -99,6 +133,13 @@ void split_node(btree tree){
 
 				parent_keys[0] = node_keys[1];
 
+				#if BTREE_DTYPE == 10
+				memcpy(kl_tmp, parent_keys_len, 16);
+				memcpy(&parent_keys_len[1], kl_tmp, 16);
+
+				parent_keys_len[0] = node_keys_len[1];
+				#endif
+
 				memcpy(parent_childs, parent_values, 2*sizeof(void*));
 				memcpy(&parent_values[1], parent_childs, 2*sizeof(void*));
 				memset(parent_childs, 0, 2*sizeof(void*));
@@ -112,16 +153,30 @@ void split_node(btree tree){
 				parent_keys[2] = parent_keys[1];
 				parent_keys[1] = node_keys[1];
 
+				#if BTREE_DTYPE == 10
+				parent_keys_len[2] = parent_keys_len[1];
+				parent_keys_len[1] = node_keys_len[1];
+				#endif
+
 				parent_values[2] = parent_values[1];
 				parent_values[1] = node_values[1];
 				
 			}else{
 				node_childs[3] = child1;
 				parent_keys[2] = node_keys[1];
+
+				#if BTREE_DTYPE == 10
+				parent_keys_len[2] = node_keys_len[1];
+				#endif
+
 				parent_values[2] = node_values[1];
 			}
 			memset(&node_keys[1], 0, 2*sizeof(btree_k_t));
 			memset(&node_values[1], 0, 2*sizeof(void*));
+
+			#if BTREE_DTYPE == 10
+			memset(&node_keys_len[1], 0, 16);
+			#endif
 
 			parent->use++;
 			if (parent->use == 3) tree = parent;
@@ -130,18 +185,41 @@ void split_node(btree tree){
 	}
 }
 
-void insert_key_to_btree(btree tree, btree_k_t key, void *value){
+void insert_key_to_btree(
+	btree tree,
+	btree_k_t key,
+	#if BTREE_DTYPE == 10
+	uint64_t length,
+	#endif
+	void *value
+){
 	uint8_t num, run=1;
-	btree_k_t *keys, k_tmp[2];
+	btree_k_t *keys;
+
+	#if BTREE_DTYPE == 10
+	uint64_t *keys_len, kl_tmp[2];
+	void *k_tmp[2];
+	#else
+	btree_k_t k_tmp[2];
+	#endif
+
 	void **values, **childs, *tmp_ptr[3];
 	while (run){
 		num = tree->use;
 		keys = tree->keys;
 		values = tree->values;
 		childs = tree->childs;
+
+		#if BTREE_DTYPE == 10
+		keys_len = tree->keys_len;
+		#endif
+
 		if (num == 0){
 			keys[0] = key;
 			values[0] = value;
+			#if BTREE_DTYPE == 10
+			keys_len[0] = length;
+			#endif
 			num++;
 			break;
 		}else{
@@ -151,6 +229,11 @@ void insert_key_to_btree(btree tree, btree_k_t key, void *value){
 						keys[x] = key;
 						values[x] = value;
 						run = 0;
+						
+						#if BTREE_DTYPE == 10
+						keys_len[x] = length;
+						#endif
+
 						num++;
 					}else{
 						tree = childs[x];
@@ -158,24 +241,32 @@ void insert_key_to_btree(btree tree, btree_k_t key, void *value){
 					break;
 				#if BTREE_DTYPE < 8
 				}else if (key < keys[x]){
-				#else
+				#elif BTREE_DTYPE < 10
 				}else if ((key - keys[x]) <= -BTREE_EPSILON){
+				#else
+				}else if (memcmp(key, keys[x], MIN(keys_len[x], length)) < 0){
 				#endif
 					if (childs[x] == NULL){
-						memcpy(k_tmp, &keys[x], 8*(2-x));
-						memcpy(&keys[x+1], k_tmp, 8*(2-x));
+						memcpy(k_tmp, &keys[x], sizeof(btree_k_t)*(2-x));
+						memcpy(&keys[x+1], k_tmp, sizeof(btree_k_t)*(2-x));
 
 						keys[x] = key;
 
-						memcpy(tmp_ptr, &values[x], 8*(2-x));
-						memcpy(&values[x+1], tmp_ptr, 8*(2-x));
+						#if BTREE_DTYPE == 10
+						memcpy(kl_tmp, &keys_len[x], 8*(2-x));
+						memcpy(&keys_len[x+1], kl_tmp, 8*(2-x));
+						keys_len[x] = length;
+						#endif
+
+						memcpy(tmp_ptr, &values[x], sizeof(btree_k_t)*(2-x));
+						memcpy(&values[x+1], tmp_ptr, sizeof(btree_k_t)*(2-x));
 
 						values[x] = value;
 
 						memset(tmp_ptr, 0, 3*sizeof(void*));
 
-						memcpy(tmp_ptr, &childs[x], 8*(3-x));
-						memcpy(&childs[x+1], tmp_ptr, 8*(3-x));
+						memcpy(tmp_ptr, &childs[x], sizeof(btree_k_t)*(3-x));
+						memcpy(&childs[x+1], tmp_ptr, sizeof(btree_k_t)*(3-x));
 
 						num++;
 						run = 0;
@@ -191,9 +282,22 @@ void insert_key_to_btree(btree tree, btree_k_t key, void *value){
 	if (num == 3) split_node(tree);
 }
 
-void *get_value_from_btree(btree tree, btree_k_t key){
-	uint64_t *keys;
+void *get_value_from_btree(
+	btree tree,
+	btree_k_t key
+	#if BTREE_DTYPE == 10
+	, uint64_t length
+	#endif
+){
 	uint8_t num;
+
+	#if BTREE_DTYPE == 10
+	uint64_t *keys_len;
+	void **keys;
+	#else
+	btree_k_t *keys;
+	#endif
+
 	void **childs, **values;
 	while (1){
 		keys = tree->keys;
@@ -201,21 +305,31 @@ void *get_value_from_btree(btree tree, btree_k_t key){
 		values = tree->values;
 		num = tree->use;
 
+		#if BTREE_DTYPE == 10
+		keys_len = tree->keys_len;
+		#endif
+
 		tree = childs[num];
 		for (uint8_t x=0; x<num; x++){
 			register void *ptr;
-			register uint64_t k = keys[x];
+			register btree_k_t k = keys[x];
 			#if BTREE_DTYPE < 8
 			if (key == k) return values[x];
 			#elif BTREE_DTYPE == 8
 			if (fabsf(key-k) <= BTREE_EPSILON) return values[x];
 			#elif BTREE_DTYPE == 9
 			if (fabs(key-k) <= BTREE_EPSILON) return values[x];
+			#elif BTREE_DTYPE == 10
+			register uint64_t kl = keys_len[x];
+			register int ret = memcmp(key, k, MIN(kl, length));
+			if (ret == 0) return values[x];
 			#endif
 			#if BTREE_DTYPE < 8
 			else if (key < k){
-			#else
+			#elif BTREE_DTYPE < 10
 			else if ((key - k) <= -BTREE_EPSILON){
+			#else
+			else if (ret < 0){
 			#endif
 				ptr = childs[x];
 				if (ptr == NULL) return NULL;
@@ -231,7 +345,13 @@ void *get_value_from_btree(btree tree, btree_k_t key){
 	return NULL;
 }
 
-static void *get_first(btree tree, btree_k_t *key, btree *node){
+static void *get_first(
+	btree tree, btree_k_t *key,
+	#if BTREE_DTYPE == 10
+	uint64_t *key_len,
+	#endif
+	btree *node
+){
 	void *child;
 	while (1){
 		child = tree->childs[0];
@@ -239,11 +359,21 @@ static void *get_first(btree tree, btree_k_t *key, btree *node){
 		tree = child;
 	}
 	key[0] = tree->keys[0];
+
+	#if BTREE_DTYPE == 10
+	key_len[0] = tree->keys_len[0];
+	#endif
 	if (node != NULL) node[0] = tree;
 	return tree->values[0];
 }
 
-static void *get_last(btree tree, btree_k_t *key, btree *node){
+static void *get_last(
+	btree tree, btree_k_t *key,
+	#if BTREE_DTYPE == 10
+	uint64_t *key_len,
+	#endif
+	btree *node
+){
 	void *child, *value = NULL;
 	uint8_t num = tree->use;
 	while (num){
@@ -255,23 +385,57 @@ static void *get_last(btree tree, btree_k_t *key, btree *node){
 	if (num > 0){
 		key[0] = tree->keys[num-1];
 		value = tree->values[num-1];
+		#if BTREE_DTYPE == 10
+		key_len[0] = tree->keys_len[num-1];
+		#endif
 		if (node != NULL) node[0] = tree;
 	}
 	return value;
 }
 
-void *get_first_value_from_btree(btree tree, btree_k_t *key){
+void *get_first_value_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+){
+	#if BTREE_DTYPE < 10
 	return get_first(tree, key, NULL);
+	#else
+	return get_first(tree, key, length, NULL);
+	#endif
 }
 
-void *get_last_value_from_btree(btree tree, btree_k_t *key){
+void *get_last_value_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+){
+	#if BTREE_DTYPE < 10
 	return get_last(tree, key, NULL);
+	#else
+	return get_last(tree, key, length, NULL);
+	#endif
 }
 
-void remove_key_from_btree(btree tree, btree_k_t key, void **value){
+void remove_key_from_btree(
+	btree tree, btree_k_t key,
+	#if BTREE_DTYPE == 10
+	uint64_t length,
+	#endif
+	void **value
+){
 	btree parent;
 	uint8_t run=1, num;
+
+	#if BTREE_DTYPE == 10
+	uint64_t *keys_len, *kl, kl_tmp[2];
+	void **keys, **k, *tmp_keys[2];
+	#else
 	btree_k_t *keys, *k, tmp_keys[2];
+	#endif
+
 	void **node_childs, **node_values, *tmp_child[3];
 	void **p_childs, **p_value;
 	while (run){
@@ -282,6 +446,10 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 		node_values = tree->values;
 		num = tree->use;
 
+		#if BTREE_DTYPE == 10
+		keys_len = tree->keys_len;
+		#endif
+
 		tree = node_childs[num];
 		for (uint8_t x=0; x<num; x++){
 			register void *ptr;
@@ -290,17 +458,29 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 			if (key == _k){
 			#elif BTREE_DTYPE == 8
 			if (fabsf(key - _k) <= BTREE_EPSILON){
-			#else
+			#elif BTREE_DTYPE == 9
 			if (fabs(key - _k) <= BTREE_EPSILON){
+			#else
+			register int ret = memcmp(key, _k, MIN(keys_len[x], length));
+			if (ret == 0){
 			#endif
 				p_childs = &node_childs[x];
 				k = &keys[x];
 				p_value = &node_values[x];
-				if (value != NULL) value[0] = node_values[x];
+
+				#if BTREE_DTYPE == 10
+				kl = &keys_len[x];
+				#endif
+
+				if (value) value[0] = node_values[x];
 				tree = current;
 				run = 0;
 				break;
+			#if BTREE_DTYPE < 10
 			}else if (key < _k){
+			#else
+			}else if (ret < 0){
+			#endif
 				tree = node_childs[x];
 				break;
 			}
@@ -330,9 +510,18 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 		memcpy(node_values, tmp_child, 2*sizeof(void*));
 		node_values[2] = NULL;
 
-		memcpy(tmp_keys, &keys[1], 16);
-		memcpy(keys, tmp_keys, 16);
+		memcpy(tmp_keys, &keys[1], sizeof(btree_k_t)*2);
+		memcpy(keys, tmp_keys, sizeof(btree_k_t)*2);
 		keys[2] = 0;
+
+		#if BTREE_DTYPE == 10
+		keys_len = tree->keys_len;
+		kl[0] = keys_len[0];
+
+		memcpy(kl_tmp, &keys_len[1], 16);
+		memcpy(keys_len, kl_tmp, 16);
+		keys_len[2] = 0;
+		#endif
 
 		num = tree->use-1;
 		if (num == 0 && node_childs[0] != NULL){
@@ -383,6 +572,13 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 		keys[num] = 0;
 		node_values[num] = NULL;
 
+		#if BTREE_DTYPE == 10
+		keys_len = tree->keys_len;
+		kl[0] = keys_len[num];
+
+		keys_len[num] = 0;
+		#endif
+
 		if (num == 0 && node_childs[0] != NULL){
 			parent = tree->parent;
 			if (parent == NULL){
@@ -413,9 +609,11 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 		#if BTREE_DTYPE < 8
 		if (keys[0] == key){
 		#elif BTREE_DTYPE == 8
-		if (fabsf(keys[0], key) <= BTREE_EPSILON){
+		if (fabsf(keys[0] - key) <= BTREE_EPSILON){
+		#elif BTREE_DTYPE == 9
+		if (fabs(keys[0] - key) <= BTREE_EPSILON){
 		#else
-		if (fabs(keys[0], key) <= BTREE_EPSILON){
+		if (memcmp(keys[0], key, MIN(keys_len[0], length)) == 0){
 		#endif
 			keys[0] = keys[1];
 			node_values[0] = node_values[1];
@@ -424,9 +622,17 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 			keys[1] = 0;
 			node_values[1] = NULL;
 			node_childs[2] = NULL;
+
+			#if BTREE_DTYPE == 10
+			keys_len[0] = keys_len[1];
+			keys_len[1] = 0;
+			#endif
 		}else{
 			keys[1] = 0;
 			node_values[1] = NULL;
+			#if BTREE_DTYPE == 10
+			keys_len[1] = 0;
+			#endif
 		}
 	}
 
@@ -446,17 +652,39 @@ void remove_key_from_btree(btree tree, btree_k_t key, void **value){
 	return;
 }
 
-void *remove_first_key_from_btree(btree tree, btree_k_t *key){
+void *remove_first_key_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+){
 	btree node;
+	#if BTREE_DTYPE == 10
+	void *value = get_first(tree, key, length, &node);
+	if (node->use > 0) remove_key_from_btree(node, key[0], length[0], NULL);
+	#else
 	void *value = get_first(tree, key, &node);
 	if (node->use > 0) remove_key_from_btree(node, key[0], NULL);
+	#endif
+	
 	return value;
 }
 
-void *remove_last_key_from_btree(btree tree, btree_k_t *key){
+void *remove_last_key_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+){
 	btree node;
+	#if BTREE_DTYPE == 10
+	void *value = get_last(tree, key, length, &node);
+	if (node->use > 0) remove_key_from_btree(node, key[0], length[0], NULL);
+	#else
 	void *value = get_last(tree, key, &node);
 	if (node->use > 0) remove_key_from_btree(node, key[0], NULL);
+	#endif
+	
 	return value;
 }
 
--- src/btree.h
+++ src/btree.h
@@ -29,27 +29,79 @@
 #define btree_k_t float
 #elif BTREE_DTYPE == 9
 #define btree_k_t double
+#elif BTREE_DTYPE == 10
+#define btree_k_t void *
 #endif
 
 typedef struct _btree_node {
 	void *parent;
 	void *childs[4];
+	
+
+	#if BTREE_DTYPE == 10
+	uint64_t keys_len[3];
+	void *keys[3];
+	#else
 	btree_k_t keys[3];
+	#endif
+
 	void *values[3];
 	uint8_t use;
 } *btree;
 
 btree allocBtree();
 
-void insert_key_to_btree(btree tree, btree_k_t key, void *value);
+void insert_key_to_btree(
+	btree tree,
+	btree_k_t key,
+	#if BTREE_DTYPE == 10
+	uint64_t length,
+	#endif
+	void *value
+);
 
-void *get_value_from_btree(btree tree, btree_k_t key);
-void *get_first_value_from_btree(btree tree, btree_k_t *key);
-void *get_last_value_from_btree(btree tree, btree_k_t *key);
+void *get_value_from_btree(
+	btree tree,
+	btree_k_t key
+	#if BTREE_DTYPE == 10
+	, uint64_t length
+	#endif
+);
 
-void remove_key_from_btree(btree tree, btree_k_t key, void **value);
-void *remove_first_key_from_btree(btree tree, btree_k_t *key);
-void *remove_last_key_from_btree(btree tree, btree_k_t *key);
+void *get_first_value_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+);
+
+void *get_last_value_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+);
+
+void remove_key_from_btree(
+	btree tree, btree_k_t key,
+	#if BTREE_DTYPE == 10
+	uint64_t length,
+	#endif
+	void **value
+);
+
+void *remove_first_key_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+);
+void *remove_last_key_from_btree(
+	btree tree, btree_k_t *key
+	#if BTREE_DTYPE == 10
+	, uint64_t *length
+	#endif
+);
 
 void freeBtree(btree tree);
 
